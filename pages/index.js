676b56e5ac0f69e5132bb1011dc8fbb682a57eb2
Seski Ramadhan <seski.ramadhan@gmail.com>
2020-11-09 16:27:37 +0700

    update my title and name

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>…</Head>
+      <p className={utilStyles.headingMd}>Full Stack Developer And DevOps</p>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
